9c3e17e8a4e35e53fac34b51f4803b5c14f2fea0
Alex Frost <zufrost.a@gmail.com>
2020-04-21 08:40:26 +0300

    fill findByName(String name) in SimpleSchoolBookRepository

--- src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
@@ -16,14 +16,42 @@ public class SimpleSchoolBookRepository implements BookRepository {
         tempSchoolBooksArray[schoolBooks.length] = (SchoolBook) book;
         // присваиваю ссылку на новый временный массив переменной schoolBooks
         schoolBooks = tempSchoolBooksArray;
-        tempSchoolBooksArray = null;
         // Если сохранение прошло успешно, метод должен вернуть true
         return true;
     }
 
     @Override
     public Book[] findByName(String name) {
-        return new Book[0];
+        /**
+         * Метод должен находить в массиве schoolBooks все книги по имени.
+         * <p>
+         * Если книги найдены - метод должен их вернуть.
+         * Если найденных по имени книг нет, должен вернуться пустой массив.
+         */
+        //сколько книг с названием name в массиве schoolBooks?
+        // создаю переменную bookCount = 0
+        int bookCount = 0;
+        // прохожу в цикле по массиву schoolBooks в поисках книги с совпадающим названием, при совпадении названия, увеличиваю переменную bookCount
+        for (Book element : schoolBooks) {
+            if (element.getName().equals(name)) {
+                bookCount++;
+            }
+        }
+        //создаю массив типа SchoolBook[] foundSchoolBooks размером bookCount
+        SchoolBook[] foundSchoolBooks = new SchoolBook[bookCount];
+        // прохожу по массиву schoolBooks повторно, и книги с совпадающими именами копирую в foundSchoolBooks
+        // переменная newArrayCounter следит за наполнением нового массива
+        int newArrayCounter = 0;
+        for (SchoolBook element : schoolBooks) {
+            if (element.getName().equals(name)) {
+                foundSchoolBooks[newArrayCounter] = element;
+                newArrayCounter++;
+            }
+        }
+        // возможны ли разные книги с совпадающими именами?? если нет, то можно упростить последнее действие
+        // возвращаю массив foundSchoolBooks
+        //return new Book[0];
+        return foundSchoolBooks;
     }
 
     @Override
