0fd9a6b5d9cbac0cce05feac4e45cd785fae91ac
Alex Frost <zufrost.a@gmail.com>
2020-04-21 08:59:46 +0300

    fill save(Author author) in SimpleAuthorService class

--- src/main/java/com/epam/izh/rd/online/service/SimpleAuthorService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleAuthorService.java
@@ -2,6 +2,7 @@ package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.entity.Author;
 import com.epam.izh.rd.online.repository.AuthorRepository;
+import com.epam.izh.rd.online.repository.SimpleAuthorRepository;
 
 public class SimpleAuthorService implements AuthorService {
     private AuthorRepository authorRepository;
@@ -15,7 +16,13 @@ public class SimpleAuthorService implements AuthorService {
 
     @Override
     public boolean save(Author author) {
-        return false;
+        /**
+         * Метод должен сохранять автора.
+         * По факту, он просто обращается к репозиторию с авторами и вызывает аналогичный метод, псоле чего возвращает результат.
+         */
+        SimpleAuthorRepository simpleAuthorRepository = new SimpleAuthorRepository();
+        simpleAuthorRepository.save(author);
+        return true;
     }
 
     @Override
